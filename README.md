f10d78b7b3dff51a005ef7dfd36737e45a6fb186
1iu <atliuning@gmail.com>
2019-11-01 11:28:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Auto Init Cluster
+This project helps users automatically initialize new clusters. (Now for CentOS 7)
---
+++ fabfile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ @desc:
+ @author: @1iu
+ @contact: atliuning@gmail.com
+ @date: 2019/11/1
+"""
